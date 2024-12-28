ecc63f5be98f0a4b6436926bfa6db1f38f14954f
Murilo Beltrame <murilobeltrame@gmail.com>
2024-12-27 21:27:03 -0300

    added base structure

---
+++ src/CapybaraEventSource.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CapybaraEventSource.Domain;
+
+public class Class1
+{
+
+}
---
+++ tests/CapybaraEventSource.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CapybaraEventSource.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
